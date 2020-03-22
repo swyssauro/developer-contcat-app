740a625f62166c6f44681e1f1b962123e1058849
swyssauro <xspynni23@gmail.com>
2020-03-22 00:58:02 -0300

    🥳 update routers

--- backend/routes/cats.js
+++ backend/routes/cats.js
@@ -3,21 +3,50 @@ const Cat = require('../models/cat');
 
 router.post('/add', async (req, res) => {
     const cat = new Cat({
+        post_name: req.body.post_name,
         url_cat: req.body.url_cat,
-        emoji_n: req.body.emoji_n
+        tag_cat: req.body.tag_cat,
+        post_p: req.body.post_p
     });
     try {
-        const saveUser = await cat.save();
+        await cat.save();
         res.send({ cat: cat._id });
     } catch (err) {
         res.status(400).send(err);
     }
 });
 
+router.route('/:id').get((req, res) => {
+    Cat.findById(req.params.id)
+    .then(cats => res.json(cats))
+    .catch(err => res.status(400).json('error' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Cat.findByIdAndDelete(req.params.id)
+    .then(cats => res.json(cats))
+    .catch(err => res.status(400).json('error' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Cat.findById(req.params.id)
+    .then(cats => {
+        cats.post_name = req.body.post_name;
+        cats.url_cat = req.body.url_cat;
+        cats.tag_cat = req.body.tag_cat;
+        cats.post_p = req.body.post_p;
+
+        cats.save()
+        .then(() => res.json(cats))
+        .catch(err => res.status(400).json('error' + err));
+    })
+    .catch(err => res.status(400).json('error' + err));
+});
+
 router.get('/', async (req, res) => {
-    User.find()
-    .then(cat => res.json(cat))
-    .catch(err => res.status(400).json('It error' + err));
+    Cat.find()
+    .then(cats => res.json(cats))
+    .catch(err => res.status(400).json('error' + err));
 });
 
 module.exports = router;
